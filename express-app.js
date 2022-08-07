97d42ddbe314aa2a0dba7bdf67b293a952c2a0c0
jesser dhieb <dhieb.jesser@esprit.tn>
2022-08-07 11:40:45 +0200

    commit

--- express-app.js
+++ express-app.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const app = express();
+const path  = require('path');
 
-app.get('/',(req,res)=>{
-    res.status(200).send('Welcome Home :)');
-})
+app.use(express.static('./navbar-app'))
+
+// app.get('/',(req,res)=>{
+//     // res.status(200).send('Welcome Home :)');
+//     res.sendFile(path.resolve(__dirname,'./navbar-app/index.html'))
+// })
 
 app.get('/about',(req,res)=>{
     res.status(200).send('about Home ;)');
