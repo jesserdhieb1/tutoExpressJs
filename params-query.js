0625881e9f6fb26385733ac3a5b4b2468277bdf9
jesser dhieb <dhieb.jesser@esprit.tn>
2022-08-07 16:32:20 +0200

    commit

--- params-query.js
+++ params-query.js
@@ -12,8 +12,22 @@ app.get('/api/products',(req,res)=>{
     })
     res.status(200).json(newProducts);
 })
+app.get('/api/products/:id_product',(req,res)=>{
+    const {id_product}=req.params;
+    console.log(req.params)
+    const singleProduct=products.find((product)=>product.id===Number(id_product));
+    if(!singleProduct){
+        return res.status(404).send('<h1>404 Not Found :(</h1>');
+    }
+    else{
+        return res.send(singleProduct);
+    }
+})
+app.get('/api/products/:id_product/reviews/:id_review',(req,res)=>{
+    res.send(`<h1>product id is ${req.params.id_product} and review id is ${req.params.id_review}</h1>`)
+    console.log(req.params)
 
-
+})
 app.listen(5000,()=>{
     console.log('Server connecting on port 5000')
 })
