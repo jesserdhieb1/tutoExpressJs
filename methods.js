a76c1af147c6f84c65a1ac547121ddce1172d840
jesser dhieb <dhieb.jesser@esprit.tn>
2022-08-08 04:55:32 +0200

    commit

--- methods.js
+++ methods.js
@@ -5,9 +5,18 @@ const data = require('./data')
 let {people} = data
 
 app.use(express.static('./methods-public'))
+app.use(express.urlencoded({extended:false}))
 
 app.get('/api/people',(req,res)=>{
     res.status(200).json({success:true,data:people})
+})
+
+app.post('/login',(req,res)=>{
+    const {name}=req.body;
+    if (name){
+        return res.status(200).send(`welcome ${name} :)`)
+    }
+    res.status(401).send('Please verify your identity :(')
 })
 
 app.listen(5000,()=>{
